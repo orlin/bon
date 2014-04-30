5a58b83dcfd0f8bb494643355e958f774130ddf8
Orlin M Bozhinov <om@soundsapiens.com>
2014-04-30 23:01:32 +0300

    ready for several test projects

--- preptest.sh
+++ preptest.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# use ./bats/bin/bats or install it somewhere else
 rm -rf bats
 git clone https://github.com/sstephenson/bats.git
-pushd test/convention
-npm install
-npm link
-popd
+# ./bats/install.sh /usr/local
+
+# install test projects
+for project in "convention"; do
+  pushd "test/$project"
+  npm install
+  npm link
+  popd
+done
